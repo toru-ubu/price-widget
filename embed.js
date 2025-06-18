4d47ad5ab94c57808e40a6b376675a830c42dfd1
toru-ubu <danceingtr32@gmail.com>
2025-06-18 12:06:39 +0900

    Update embed.js

--- embed.js
+++ embed.js
@@ -2,48 +2,41 @@
   const html = `
     <!-- ここに価格表のHTMLを貼る --><table style="width:100%; border-collapse: collapse;" border="1">
   <tr>
-    <th>商品名</th>
-    <th>通常価格</th>
-    <th>セール価格</th>
-    <th>割引率</th>
-    <th>今が買い時？</th>
+    <th style="width:60%;">商品名</th>
+    <th style="width:13%;">通常価格</th>
+    <th style="width:13%;">セール価格</th>
+    <th style="width:14%;">割引率</th>
   </tr>
 <tr>
-  <td><a href="https://www.dospara.co.jp/TC30/MC18484-SN4783.html" target="_blank">GALLERIA RM7R-R56T 7700搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
-  <td>¥224,780</td>
-  <td>¥224,780</td>
-  <td>0.0%</td>
-  <td></td>
+  <td style="width:60%;"><a href="https://www.dospara.co.jp/TC30/MC18484-SN4783.html" target="_blank">GALLERIA RM7R-R56T 7700搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
+  <td style="width:13%;">¥224,780</td>
+  <td style="width:13%;">¥224,780</td>
+  <td style="width:14%;">0.0%</td>
 </tr><tr>
-  <td><a href="https://www.dospara.co.jp/TC30/MC18485-SN4737.html" target="_blank">GALLERIA RM7R-R57 5700X搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
-  <td>¥281,480</td>
-  <td>¥281,480</td>
-  <td>0.0%</td>
-  <td></td>
+  <td style="width:60%;"><a href="https://www.dospara.co.jp/TC30/MC18485-SN4737.html" target="_blank">GALLERIA RM7R-R57 5700X搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
+  <td style="width:13%;">¥281,480</td>
+  <td style="width:13%;">¥281,480</td>
+  <td style="width:14%;">0.0%</td>
 </tr><tr>
-  <td><a href="https://www.dospara.co.jp/TC30/MC18486-SN4767.html" target="_blank">GALLERIA XA7C-R56T Intel Core Ultra/RTX5060Ti 16GB搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
-  <td>¥325,480</td>
-  <td>¥325,480</td>
-  <td>0.0%</td>
-  <td></td>
+  <td style="width:60%;"><a href="https://www.dospara.co.jp/TC30/MC18486-SN4767.html" target="_blank">GALLERIA XA7C-R56T Intel Core Ultra/RTX5060Ti 16GB搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
+  <td style="width:13%;">¥325,480</td>
+  <td style="width:13%;">¥325,480</td>
+  <td style="width:14%;">0.0%</td>
 </tr><tr>
-  <td><a href="https://www.dospara.co.jp/TC30/MC18488-SN4757.html" target="_blank">GALLERIA XA7C-R58 Intel Core Ultra搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
-  <td>¥545,180</td>
-  <td>¥545,180</td>
-  <td>0.0%</td>
-  <td></td>
+  <td style="width:60%;"><a href="https://www.dospara.co.jp/TC30/MC18488-SN4757.html" target="_blank">GALLERIA XA7C-R58 Intel Core Ultra搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
+  <td style="width:13%;">¥545,180</td>
+  <td style="width:13%;">¥545,180</td>
+  <td style="width:14%;">0.0%</td>
 </tr><tr>
-  <td><a href="https://www.dospara.co.jp/TC30/MC18483-SN4821.html" target="_blank">GALLERIA RM5R-R56T 4500搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
-  <td>¥181,980</td>
-  <td>¥181,980</td>
-  <td>0.0%</td>
-  <td></td>
+  <td style="width:60%;"><a href="https://www.dospara.co.jp/TC30/MC18483-SN4821.html" target="_blank">GALLERIA RM5R-R56T 4500搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
+  <td style="width:13%;">¥181,980</td>
+  <td style="width:13%;">¥181,980</td>
+  <td style="width:14%;">0.0%</td>
 </tr><tr>
-  <td><a href="https://www.dospara.co.jp/TC30/MC18483.html" target="_blank">GALLERIA RM5R-R56T 4500搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
-  <td>¥181,980</td>
-  <td>¥181,980</td>
-  <td>0.0%</td>
-  <td></td>
+  <td style="width:60%;"><a href="https://www.dospara.co.jp/TC30/MC18483.html" target="_blank">GALLERIA RM5R-R56T 4500搭載 夏の準備応援祭特別モデル 『Minecraft: Java ＆ Bedrock Edition for PC、PC Game Pass同梱版』</a></td>
+  <td style="width:13%;">¥181,980</td>
+  <td style="width:13%;">¥181,980</td>
+  <td style="width:14%;">0.0%</td>
 </tr></table>
   <p style="margin-top:10px; font-size:0.9em;">
     提供元：<a href="https://kidukeba.co.jp/" target="_blank" rel="dofollow">合同会社気づけば</a>
